81617514b2eef9290e87cae5d3fb4f2239312ff4
MetaEmpire <kfejeran@uw.edu>
2015-01-29 07:46:13 +0000

    client fleshed out. ready for testing both, need arduino?

--- blink_client.py
+++ blink_client.py
@@ -2,9 +2,13 @@
 blink_client.py
 
 Communicates with a server to control a remote Arduino LED.
+
+By Keenan Fejeran
+1/28/2015
 '''
 
 import zmq
+import time
 
 '''
 context
@@ -15,3 +19,18 @@ user input
 zmq send OVER THE INTERNET
 print server response
 '''
+
+#Create a context and socket
+context = zmq.Context()
+socket = context.socket(zmq.PAIR)
+
+#Connect socket to server
+socket.connect("tcp://localhost:5556")
+
+#Send messages to server and listen to response
+
+while True:
+	socket.send("C2S" + time.ctime())
+	msg = socket.recv()
+	print msg
+	time.sleep(1)
