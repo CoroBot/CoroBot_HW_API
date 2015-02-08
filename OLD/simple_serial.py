2e80e37a3af5075cdb319de7dc61b06bcf836834
MetaEmpire <kfejeran@uw.edu>
2015-02-07 16:20:09 -0800

    a simple serial python script to talk to anything over serial

---
+++ OLD/simple_serial.py
@@ -0,0 +1,16 @@
+#simple_serial.py
+
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 0) #change to tiva board?
+
+i = 0
+
+while i<10:
+	ser.write("Hello")
+	#msg = ser.readline() #?
+	time.sleep(1)
+	i = i+1
+	
+ser.close()
